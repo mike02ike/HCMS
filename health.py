e05b75835a981ed61e0279cc94479aa58a5f9f95
Mike Ike <mike02ike@gmail.com>
2023-08-18 11:18:35 -0500

    First file

---
+++ health.py
@@ -0,0 +1,22 @@
+class Patient():
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+        self.date_of_birth = None
+        self.gender = None
+        self.blood_type = None
+        self.medical_history = None
+        self.height = None
+        self.weight = None
+        self.emergency_contact_info = None
+        self.insurance_information = None
+        self.current_medications = None
+        self.diagnosis = None
+        self.treatment_plan = None
+        self.follow_up_appointments = None
+        self.bmi = None
+        self.allergies = None
+        self.symptoms = None
+        self.family_history = None
+        self.primary_care_physician = None
+        self.lab_results = None
